84b9d64caf59574bfa90b59aa25ba2532a18be0b
Gaks3 <dwi66116@gmail.com>
2024-12-31 19:01:20 +0700

    feat: create sign up page using clerk
    The page is implemented using the SignUp component from the Clerk library, simplifying the codebase while reducing complexity. This integration ensures an intuitive and efficient user registration process.

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,34 @@
+import { ClerkLoaded, ClerkLoading, SignUp } from '@clerk/nextjs'
+import { Loader2Icon } from 'lucide-react'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2 relative'>
+      <div className='h-full lg: flex flex-col items-center justify-center px-4'>
+        <div className='text-center space-y-4 pt-16'>
+          <h1 className='font-bold text-3xl text-[#2E2E47]'>Welcome Back</h1>
+          <p className='text-base text-[#7E8CA0]'>
+            Log in or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className='flex items-center justify-center mt-8'>
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2Icon className='animate-spin text-muted-foreground' />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className='h-full max-h-screen sticky top-0 bg-blue-600 hidden lg:flex items-center justify-center'>
+        <Image
+          src={'/logo-white.svg'}
+          alt='Logo Image'
+          width={100}
+          height={100}
+        />
+      </div>
+    </div>
+  )
+}
